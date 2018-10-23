75405a8d745ce564b3808c01fed707afeb485920
tayanovskii <tayanovskii@gmail.com>
2018-10-23 10:17:12 +0300

    UserInterface

---
+++ ImageProcessor/ImageProcessor/Extensions/UserInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageProcessor.Extensions
+{
+    public class UserInterface
+    {
+        public static void PrintFunctional()
+        {
+            Console.WriteLine("Введите номер необходимой команды (1,2,3 или 4):");
+            Console.WriteLine("1.Переименовать изображения в соответствии с датой сьемки");
+            Console.WriteLine("2.Добавить на изображение отметку, когда фото было сделано");
+            Console.WriteLine("3.Сортировка изображений по папкам по годам");
+            Console.WriteLine("4.Сортировка изображений по папкам по месту сьемки");
+            
+        }
+
+        public static string ReadUserInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public static short UserInputToInt(string userInput)
+        {
+            return short.TryParse(userInput, out var userInputToInt) ? userInputToInt : (short) 0;
+        }
+
+        public static void PrintRequestPath()
+        {
+            Console.WriteLine("Введите путь к папке изображений (с:\\images\\):");
+        }
+    }
+}
---
+++ ImageProcessor/ImageProcessor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageProcessor.Extensions;
+
+namespace ImageProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface.PrintFunctional();
+            var userInput = UserInterface.ReadUserInput();
+            var userInputToInt = UserInterface.UserInputToInt(userInput);
+            Console.WriteLine(userInputToInt);
+            UserInterface.PrintRequestPath();
+            Console.ReadKey();
+        }
+    }
+}
