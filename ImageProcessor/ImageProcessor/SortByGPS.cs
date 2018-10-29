d2f63bb0610150201f214d1775cd8da91a702085
tayanovskii <tayanovskii@gmail.com>
2018-10-29 17:23:10 +0300

    29.10.2018

--- ImageProcessor/ImageProcessor/SortByGPS.cs
+++ ImageProcessor/ImageProcessor/SortByGPS.cs
@@ -12,6 +12,7 @@ using System.Web;
 using System.Xml;
 using System.Xml.Linq;
 using ImageProcessor.Extensions;
+using Newtonsoft.Json;
 
 namespace ImageProcessor
 {
@@ -43,32 +44,60 @@ namespace ImageProcessor
 
             //AIzaSyCMKknJ0jyVCYVzRSjbMDGLuYx-qxxRqyw
 
+            //43e2be5e-2161-4ec8-aafd-6c07a01a7fbc
+
             string coordinate = "32.797821,-96.781720"; //Console.ReadLine();
 
-            XmlDocument xDoc = new XmlDocument();
-           // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
-            xDoc.Load($"https://maps.googleapis.com/maps/api/geocode/xml?latlng={coordinate}&key=sadfwet56346tyeryhretu6434tertertreyeryeryE1");
+            var uri =
+                $"https://geocode-maps.yandex.ru/1.x/?apikey=43e2be5e-2161-4ec8-aafd-6c07a01a7fbc&format=json&geocode={coordinate}";
 
-            XmlNodeList xNodelst = xDoc.GetElementsByTagName("result");
-            XmlNode xNode = xNodelst.Item(0);
-            string FullAddress = xNode.SelectSingleNode("formatted_address").InnerText;
-            string Number = xNode.SelectSingleNode("address_component[1]/long_name").InnerText;
-            string Street = xNode.SelectSingleNode("address_component[2]/long_name").InnerText;
-            string Village = xNode.SelectSingleNode("address_component[3]/long_name").InnerText;
-            string Area = xNode.SelectSingleNode("address_component[4]/long_name").InnerText;
-            string County = xNode.SelectSingleNode("address_component[5]/long_name").InnerText;
-            string State = xNode.SelectSingleNode("address_component[6]/long_name").InnerText;
-            string Zip = xNode.SelectSingleNode("address_component[8]/long_name").InnerText;
-            string Country = xNode.SelectSingleNode("address_component[7]/long_name").InnerText;
-            Console.WriteLine("Full Address: " + FullAddress);
-            Console.WriteLine("Number: " + Number);
-            Console.WriteLine("Street: " + Street);
-            Console.WriteLine("Village: " + Village);
-            Console.WriteLine("Area: " + Area);
-            Console.WriteLine("County: " + County);
-            Console.WriteLine("State: " + State);
-            Console.WriteLine("Zip: " + Zip);
-            Console.WriteLine("Country: " + Country);
+
+            //WebRequest request = WebRequest.Create(uri);
+            //request.Credentials = CredentialCache.DefaultCredentials;
+            //request.Method = "GET";
+            //request.ContentType = "application/json; charset=utf-8";
+
+            //using (var response = request.GetResponse())
+            //using (var dataStream = response.GetResponseStream())
+            //{
+            //    StreamReader reader = new StreamReader(dataStream);
+            //    string responseFromServer = reader.ReadToEnd();
+            //    Console.WriteLine(responseFromServer);
+            //}
+
+            WebClient client = new WebClient();
+            var jsonstring = client.DownloadString(uri);
+            dynamic dynObj = JsonConvert.DeserializeObject(jsonstring);
+            string requst = dynObj.request;
+
+            //Console.WriteLine(dynObj);
+
+
+
+            // XmlDocument xDoc = new XmlDocument();
+            //// https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+            // xDoc.Load($"https://maps.googleapis.com/maps/api/geocode/xml?latlng={coordinate}&key=sadfwet56346tyeryhretu6434tertertreyeryeryE1");
+
+            // XmlNodeList xNodelst = xDoc.GetElementsByTagName("result");
+            // XmlNode xNode = xNodelst.Item(0);
+            // string FullAddress = xNode.SelectSingleNode("formatted_address").InnerText;
+            // string Number = xNode.SelectSingleNode("address_component[1]/long_name").InnerText;
+            // string Street = xNode.SelectSingleNode("address_component[2]/long_name").InnerText;
+            // string Village = xNode.SelectSingleNode("address_component[3]/long_name").InnerText;
+            // string Area = xNode.SelectSingleNode("address_component[4]/long_name").InnerText;
+            // string County = xNode.SelectSingleNode("address_component[5]/long_name").InnerText;
+            // string State = xNode.SelectSingleNode("address_component[6]/long_name").InnerText;
+            // string Zip = xNode.SelectSingleNode("address_component[8]/long_name").InnerText;
+            // string Country = xNode.SelectSingleNode("address_component[7]/long_name").InnerText;
+            // Console.WriteLine("Full Address: " + FullAddress);
+            // Console.WriteLine("Number: " + Number);
+            // Console.WriteLine("Street: " + Street);
+            // Console.WriteLine("Village: " + Village);
+            // Console.WriteLine("Area: " + Area);
+            // Console.WriteLine("County: " + County);
+            // Console.WriteLine("State: " + State);
+            // Console.WriteLine("Zip: " + Zip);
+            // Console.WriteLine("Country: " + Country);
 
             //return locationName;
 
